7d7e4b2d49b3fe8f63c1af0161c745d7c49bfd1f
jordhi404 <tejosaputro22@gmail.com>
2025-05-21 09:54:50 +0700

    Setting url di server, 21/05/2025

--- resources/js/qr-payment.js
+++ resources/js/qr-payment.js
@@ -8,8 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.Echo.channel('paid.payment.5ucc355').listen('.paid.payment', (e) => {
         // console.log('Broadcast received:', e);
-        window.location.href = "/payment-success"; // local side
-        // window.location.href = "/apm/payment-success"; // server side
+        // window.location.href = "/payment-success"; // local side
+        window.location.href = "/apm/payment-success"; // server side
 
         const payload = e.data;
 
