361d2ef32c3df9dbbdc393fb561bdd1d370fea36
jordhi404 <tejosaputro22@gmail.com>
2025-05-31 04:51:35 +0700

    penyesuaian kecil untuk durasi carousel iklan

--- resources/views/layouts/templates.blade.php
+++ resources/views/layouts/templates.blade.php
@@ -28,13 +28,13 @@
 
                 <!-- Isi Carousel -->
                 <div class="carousel-inner">
-                    <div class="carousel-item active" data-bs-interval="7000"> <!-- durasi per gambar -->
+                    <div class="carousel-item active" data-bs-interval="5000"> <!-- durasi per gambar -->
                         <img src="images/promo-1.jpg" class="d-block w-100" alt="item1">
                     </div>
-                    <div class="carousel-item" data-bs-interval="7000">
+                    <div class="carousel-item" data-bs-interval="5000">
                         <img src="images/promo-2.jpg" class="d-block w-100" alt="item2">
                     </div>
-                    <div class="carousel-item" data-bs-interval="7000">
+                    <div class="carousel-item" data-bs-interval="5000">
                         <img src="images/promo-3.jpeg" class="d-block w-100" alt="item3">
                     </div>
                 </div>
